86b16fef00e2e699ab161ff7cf0a0fdc35f7add0
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-30 16:01:32 +0200

    Finish script file

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ var button = document.getElementById('get-joke');
 button.addEventListener('click', function(){
   getJoke();
 });
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
+
+getJoke();
+
